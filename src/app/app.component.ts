2711703d0df8b992357b840132f8b4debaacd300
Carlos Espinoza <carlos.espinoza23@unmsm.edu.pe>
2024-04-05 12:21:07 -0500

    Agregando Lab_1

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular-app';
+  nombre: string = 'Carlos'
+  edad: number = 21
+  telefono: null = null
+  direccion: undefined = undefined
+  otros: any;
+  /*
+  car: CarModel = {
+    marca: 'Ford',
+    modelo: 'Focus',
+    ano: 2022
+  }
+  
+
+  listaCar: Array<CarModel> = [{
+    marca: 'Nisan',
+    modelo: 'Salon',
+    ano: 2020
+  },{
+    marca: 'Toyota',
+    modelo: 'Silver 2000',
+    ano: 2021
+  }
+]*/
+}
+
+/*
+interface CarModel {
+  marca: string;
+  modelo: string;
+  ano?: number;
+}*/
